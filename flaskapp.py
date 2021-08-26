ff81ccf1300e18d09dade15aab7f86c1014c94fb
Ryan <rrmhearts@gmail.com>
2021-08-26 11:18:20 -0400

    wtf forms

--- flaskapp.py
+++ flaskapp.py
@@ -1,18 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from forms import Todo
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'password'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('home.j2', list_of_names=['Elon', 'Jeff', 'Ash'])
+    if request.method == 'POST':
+        data = request.form
+        firstname = data['firstname']
+        lastname = data['lastname']
+        print('Received', data)
+        return redirect(url_for('name', firstname=firstname))
 
-@app.route('/about')
+    return render_template('home.j2', \
+        request_method=request.method, \
+        list_of_names=['Elon', 'Jeff', 'Ash'])
+
+@app.route('/name/<string:firstname>')
+def name(firstname):
+    return f'{firstname}'
+
+@app.route('/todo', methods=['GET', 'POST'])
+def todo():
+    todo_form = Todo()
+    if todo_form.validate_on_submit():
+        print(todo_form.content.data)
+        return redirect('/')
+
+    return render_template('todo.j2', form=todo_form)
+
+@app.route('/about', methods=['GET', 'POST'])
 def about():
     return render_template('about.j2')
 
-@app.route('/<string:name>')
-def greet(name):
-    return f'Hello {name}'
+@app.route('/<string:named>')
+def greet(named):
+    return f'Hello {named}'
 
 if __name__ == '__main__':
     app.run(debug=True)
